121ab8b1d892f0761a7291f30eb3512075bbe424
sendygit <sendygg23@gmail.com>
2021-10-21 14:34:28 +0700

    push env

--- app/Http/Controllers/TransactionController.php
+++ app/Http/Controllers/TransactionController.php
@@ -276,7 +276,7 @@ class TransactionController extends Controller
         } catch (\Exception $e) {
             return response()->json([
                 'success' => false,
-                'message' => "Failed"
+                'message' => "Data tidak ditemukan"
             ]);
     }
     }
